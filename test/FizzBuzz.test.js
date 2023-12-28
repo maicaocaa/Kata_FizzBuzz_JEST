8f8ce8e2a52463bad14b8d53c37d29b64d1e4281
maicaocaaa <maria.caocaa@gmail.com>
2023-12-28 11:22:58 +0100

    first commit

---
+++ test/FizzBuzz.test.js
@@ -0,0 +1,12 @@
+
+// imporatr libreria
+const {expecte = required ("@jest/globas");
+// nos permite importar la libreria
+
+/**
+ * featura para saber si eun numero es primo
+ * 1 cuando es divisible por 3 es fizz
+ * cuando es divisible por 5 es buzz
+ * cuando es divisible  entre 3 y 5 devuelve fizzbuzz
+ * cuando no es dibisible, devuelve el numero tal cua
+ */
