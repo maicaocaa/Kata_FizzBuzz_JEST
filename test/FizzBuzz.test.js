12066aadec82093141de73d82ea2d0107f102575
maicaocaaa <maria.caocaa@gmail.com>
2023-12-28 16:24:27 +0100

    test jest fizzbuzz ok

--- test/FizzBuzz.test.js
+++ test/FizzBuzz.test.js
@@ -1,9 +1,5 @@
-
-// imporatr libreria
 const {expect} = require ("@jest/globals");
 const FizzBuzz = require ("../FizzBuzz.js");
-
-// nos permite importar la libreria
 
 /**
  * featura para saber si un numero es primo
@@ -17,34 +13,24 @@ describe ('Tests que validan los multiplos de 3 y 5',() => {
     const fizz_buzz = new FizzBuzz();
 
      test (`Test devuelve Fizz cuando es divisible entre 3`,()=>{
-
-        //Arrange prepara escenario,  la entrada y salida
         const numero_ingresado = 3;
         const respuesta_esperada= "Fizz"
 
         const respuesta_recibida = fizz_buzz.validar_primos_3_5(numero_ingresado);
-
-        //ASSERT comprobar el escenario 
         expect (respuesta_recibida).toBe(respuesta_esperada);
      });
 
-
      test (`Test devuelve Buzz cuando es divisible entre 5`,()=>{
-
-        //Arrange prepara escenario,  la entrada y salida
         const numero_ingresado = 5;
         const respuesta_esperada= "Buzz"
 
-        // Act ejecuta el escenario
         const respuesta_recibida = fizz_buzz.validar_primos_3_5(numero_ingresado);
 
-        //ASSERT comprobar el escenario 
         expect (respuesta_recibida).toBe(respuesta_esperada);
      });
 
 
      test (`Test devuelve FizzBuzz cuando es divisible entre 3 y 5`,()=>{
-
         const numero_ingresado = 15;
         const respuesta_esperada= "FizzBuzz"
 
@@ -55,12 +41,11 @@ describe ('Tests que validan los multiplos de 3 y 5',() => {
 
      
      test (`Test devuelve el numero cuando no es divisible ni por 3 ni por 5`,()=>{
-      
-      const numero_ingresado = 1;
-      const respuesta_esperada= numero_ingresado;
+        const numero_ingresado = 1;
+        const respuesta_esperada= numero_ingresado;
 
-      const respuesta_recibida = fizz_buzz.validar_primos_3_5(numero_ingresado);
-
-      expect (respuesta_recibida).toBe(respuesta_esperada);
+        const respuesta_recibida = fizz_buzz.validar_primos_3_5(numero_ingresado);
+        
+        expect (respuesta_recibida).toBe(respuesta_esperada);
    });
 })
